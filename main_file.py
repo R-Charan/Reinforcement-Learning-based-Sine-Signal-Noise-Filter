80d139012d0351ada12a980032df0d9e6725d359
R Charan <rcharanbhardhwaj@gmail.com>
2023-11-17 12:54:35 +0530

    init.py file

--- main_file.py
+++ main_file.py
@@ -1,18 +1,31 @@
+
+
 from environement import environment
 import numpy as np
 from rlmodels import build_agent, build_model
 from tensorflow.keras.optimizers import Adam
-env = environment()
-states = np.shape(env.state)
-print(states, " state value")
-print("observation space ", np.shape(env.signal))
-actions = len(env.action_space)
 
+if __name__ == "__main__":
 
-model = build_model(states,actions)
+    # Create an object of the environment class
+    env = environment()
+    # Define the states of the model
+    states = np.shape(env.state)
+    print(states, " state value")
+    print("observation space ", np.shape(env.signal))
+    # Define the actions
+    actions = len(env.action_space)
 
-dqn = build_agent(model, actions)
-dqn.compile(Adam(learning_rate = 5e-4) , metrics = ['mae'])
-dqn.fit(env, nb_steps=10000, visualize= False, verbose = 1)
-dqn.save_weights("/content/drive/MyDrive/Intern/trial/", overwrite = True)
-print(env.state)
+    # Build the RL model from rlmodel.py file
+    model = build_model(states,actions)
+
+    # Defining the parameters of the Deep Q Network
+    dqn = build_agent(model, actions)
+    dqn.compile(Adam(learning_rate = 5e-4) , metrics = ['mae'])
+
+    # Running the DQN Model
+    dqn.fit(env, nb_steps=10000, visualize= False, verbose = 1)
+
+    # Saving the weights
+    dqn.save_weights("/content/drive/MyDrive/Intern/trial/", overwrite = True)
+    print(env.state)
